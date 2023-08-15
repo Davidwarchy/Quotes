5fa8365d94523bea837374636bc84a57074be3e0
David Warutumo <davidwarchy@gmail.com>
2023-08-15 07:07:40 +0300

    custom quote as cmdline argument

--- screenshot.js
+++ screenshot.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+(async (message) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
@@ -10,11 +10,19 @@ const puppeteer = require('puppeteer');
   // Wait for any additional resources to load
   await page.waitForTimeout(1000); // Adjust the timeout as needed
 
-  // Capture a screenshot of the card element
+  // Replace the text in the quote element with the provided message
+  await page.evaluate((message) => {
+    const quoteElement = document.querySelector('#quote-holder');
+    if (quoteElement) {
+      quoteElement.textContent = message;
+    }
+  }, message);
+
+  // Capture a screenshot of the modified card element
   const cardElement = await page.$('.card'); // Change the selector as needed
   if (cardElement) {
     await cardElement.screenshot({ path: 'screenshot.png' });
   }
 
   await browser.close();
-})();
+})(process.argv[2]); // Get the message from the command line argument
